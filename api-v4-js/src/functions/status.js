e80f754c6f7aa2cbbc90fefb813f649292f0b7ef
Dina Berry (SHE/HER) <Dina.Berry@microsoft.com>
2023-05-02 19:57:22 +0000

    Azure Function v4 JS

---
+++ api-v4-js/src/functions/status.js
@@ -0,0 +1,22 @@
+const { app } = require("@azure/functions");
+
+async function status(request, context) {
+    context.log(`Http function processed request for url "${request.url}"`);
+
+    return {
+        status: 200,
+        jsonBody: {
+            env: process.env,
+            requestHeaders: request.headers
+        }
+    };
+};
+
+app.http('status', {
+    route: "status",
+    methods: ['GET'],
+    authLevel: 'anonymous',
+    handler: status
+});
+
+module.exports = status
