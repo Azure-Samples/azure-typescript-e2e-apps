a8be54885a7c3fcff2423bfa67e108b29bddda6e
Dina Berry <dinaberry@outlook.com>
2023-05-19 09:02:17 -0700

    connection test

--- lib/src/connection-test.ts
+++ lib/src/connection-test.ts
@@ -3,41 +3,68 @@ import { v4 as uuidv4 } from 'uuid';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-/*
+function withTimeout<T>(promise: Promise<T>, timeoutMs: number): Promise<T> {
+  return new Promise((resolve, reject) => {
+    const timeout = setTimeout(() => {
+      reject(new Error('Timeout exceeded'));
+    }, timeoutMs);
 
-This should eventually fail if resource isn't there or permissions are wrong. 
+    promise
+      .then((result) => {
+        clearTimeout(timeout);
+        resolve(result);
+      })
+      .catch((error) => {
+        clearTimeout(timeout);
+        reject(error);
+    });
+  });
+}
 
-TBD: Why doesn't it fail for those circumstances?
-
-*/
-
-async function main() {
-  const name = process.env.AZURE_COSMOSDB_NAME as string;
-  const key = process.env.AZURE_COSMOSDB_KEY as string;
+async function insertData(
+  resourceName: string,
+  key: string,
+  data: any,
+  databaseName = 'test',
+  containerName = 'test'
+): Promise<any> {
   const client = new CosmosClient({
-    endpoint: `https://${name}.documents.azure.com`,
-    key,
-    connectionPolicy: {
-      requestTimeout: 1000,
-      retryOptions: {
-        maxRetryAttemptCount: 0,
-        fixedRetryIntervalInMilliseconds: 0,
-        maxWaitTimeInSeconds: 1000
-      }
-    }
+    endpoint: `https://${resourceName}.documents.azure.com`,
+    key
   });
+  console.log('client created');
   const { database } = await client.databases.createIfNotExists({
-    id: 'test'
+    id: databaseName
   });
+  console.log('database created');
   const { container } = await database.containers.createIfNotExists({
-    id: 'test'
+    id: containerName
   });
+  console.log('container created');
   const { resource: item } = await container.items.create({
     id: uuidv4().toString(),
-    name: 'test'
+    ...data
   });
-  console.log('Created item:', item);
+  console.log('item created');
+  return item;
 }
-main().catch((error: any) => {
-  console.error('Error running connection test:', error);
-});
+
+
+const name = process.env.AZURE_COSMOSDB_NAME as string;
+const key = process.env.AZURE_COSMOSDB_KEY as string;
+const timeoutMs = 10000;
+const wrappedPromise = withTimeout(
+  insertData(name, key, { name: 'hello', age: 21 }),
+  timeoutMs
+);
+
+wrappedPromise
+  .then((result) => {
+  console.log('Operation finished ', result); // Data fetched successfully
+  process.exit(0);
+  })
+  .catch((error) => {
+  console.log('Operation didn\'t finish', error.message); // Timeout exceeded
+  process.exit(1);
+  });
+
