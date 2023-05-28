eb8a662519da75afaf9ad548ed8fdc82d8c5b36e
Dina Berry (SHE/HER) <Dina.Berry@microsoft.com>
2023-05-28 07:16:01 -0700

    cli works with 1 question

--- cli-openai/src/index.ts
+++ cli-openai/src/index.ts
@@ -2,10 +2,20 @@ import { Command, OptionValues } from 'commander';
 import * as dotenv from 'dotenv';
 import { readFileSync } from 'fs';
 import path from 'path';
-import { createInterface } from 'readline';
 import { checkRequiredEnvParams } from './settings';
+import OpenAIConversationManager, {
+  OpenAiResponse
+} from '@azure-typescript-e2e-apps/lib-openai';
 
-import readline from 'readline';
+import readline from 'node:readline/promises';
+dotenv.config();
+
+const openAiManager = new OpenAIConversationManager(
+  process.env.AZURE_OPENAI_ENDPOINT as string,
+  process.env.AZURE_OPENAI_API_KEY as string,
+  process.env.AZURE_OPENAI_DEPLOYMENT as string,
+  process.env.AZURE_OPENAI_API_VERSION as string
+);
 
 const program = new Command();
 const rl = readline.createInterface({
@@ -37,25 +47,46 @@ program.description('Start a conversation').action(async () => {
 
   /* eslint-disable-next-line no-constant-condition */
   while (true) {
-    const answer: string = await askQuestion('What would you like to ask? ');
-
+    const yourQuestion: string = await askQuestion(
+      'What would you like to ask? '
+    );
     // Print response
-    console.log(`You said: ${answer}`);
+    console.log(`\nYOU: ${yourQuestion}`);
 
     // Exit if user types 'exit'
-    if (answer.toLowerCase() === 'exit') {
+    if (yourQuestion.toLowerCase() === 'exit') {
       console.log('Goodbye!');
       process.exit();
     }
+
+    await getAnswer(yourQuestion);
   }
 });
 
 async function askQuestion(question: string): Promise<string> {
-  return new Promise((resolve) => {
-    rl.question(question, (answer) => {
-      resolve(answer);
-    });
-  });
+  return await rl.question(question);
+}
+async function getAnswer(question: string): Promise<void> {
+  const { status, data, error }: OpenAiResponse =
+    await openAiManager.OpenAiConverationStep(question);
+
+  if (Number(status) > 299) {
+    process.stdout.write(
+      `Conversation step request error: ${error?.message || 'unknown'}`
+    );
+    process.exit();
+  }
+
+  if (data?.choices[0]?.message) {
+    process.stdout.write(
+      `\n\nASSISTANT:\n\n${data?.choices[0].message.content}\n\n>(enter response or "exit" to quit): `
+    );
+  } else {
+    // no response from assistant
+    process.stdout.write(
+      `\n\nASSISTANT:\n\nNo response provided.\n\n>(enter response or "exit" to quit): `
+    );
+  }
 }
 
 program.parse(process.argv);
