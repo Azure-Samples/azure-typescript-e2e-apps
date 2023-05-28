daed0dc398a8d155560d4564c576b7e7e0653e22
Dina Berry (SHE/HER) <Dina.Berry@microsoft.com>
2023-05-28 05:27:04 -0700

    loop w/o recursion

--- cli-openai/src/index.ts
+++ cli-openai/src/index.ts
@@ -1,9 +1,3 @@
-import OpenAIConversationManager, {
-  OpenAiAppConfig,
-  OpenAiRequest,
-  OpenAiRequestConfig,
-  OpenAiResponse
-} from '@azure-typescript-e2e-apps/lib-openai';
 import { Command, OptionValues } from 'commander';
 import * as dotenv from 'dotenv';
 import { readFileSync } from 'fs';
@@ -19,21 +13,42 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
+program
+  .name('conversation')
+  .description(
+    `A conversation loop
+
+        Examples: 
+        index.js -d 'myfile.txt' -e '.env'        Start convo with text from file with settings from .env file
+    `
+  )
+  .option(
+    '-d, --dataFile <filename>',
+    'Read content from a file. If both input and data file are provided, both are sent with initial request. Only input is sent with subsequent requests.'
+  )
+  .option(
+    '-e, --envFile <filename>',
+    'Load environment variables from a file. Prefer .env to individual option switches. If both are sent, .env is used only.'
+  )
+  .helpOption('-h, --help', 'Display help');
 
 program
   .command('start')
   .description('Start a conversation')
   .action(async () => {
-    console.log('Welcome to the conversation!');
+    console.log('Welcome to the OpenAI conversation!');
 
     /* eslint-disable-next-line no-constant-condition */
     while (true) {
-      const answer: string = await askQuestion('What would you like to say? ');
+      const answer: string = await askQuestion('What would you like to ask? ');
+
+      // Print response
       console.log(`You said: ${answer}`);
 
+      // Exit if user types 'exit'
       if (answer.toLowerCase() === 'exit') {
         console.log('Goodbye!');
-        break;
+        process.exit();
       }
     }
   });
