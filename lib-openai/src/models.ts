8886bb07d7f858e555a12ce1ab5253bd6acee7e8
Dina Berry <diberry@microsoft.com>
2023-05-30 09:32:23 -0700

    Diberry/feature/0525 lib openai (#24)
    ---------
    
    Co-authored-by: Dina Berry (SHE/HER) <Dina.Berry@microsoft.com>

---
+++ lib-openai/src/models.ts
@@ -0,0 +1,57 @@
+export type OpenAiRequestConfig = {
+  max_tokens: number;
+  temperature: number;
+  top_p: number;
+  frequency_penalty: number;
+  presence_penalty: number;
+  stop: string[] | string;
+};
+export type OpenAiAppConfig = {
+  apiVersion: string;
+  endpoint: string;
+  apiKey: string;
+  deployment: string;
+};
+
+export type Role = 'user' | 'assistant' | 'system';
+export type Message = {
+  role: Role | string;
+  content: string;
+};
+export type OpenAiConversation = {
+  systemPrompt: Message;
+  messages: Message[];
+};
+export type OpenAiRequest = {
+  appConfig: OpenAiAppConfig;
+  requestConfig: OpenAiRequestConfig;
+  conversation: OpenAiConversation;
+};
+export type Choice = {
+  index: number;
+  finish_reason: string;
+  message: Message;
+};
+export type Usage = {
+  completion_tokens: number;
+  prompt_tokens: number;
+  total_tokens: number;
+};
+export type OpenAiErrorResponse = {
+  message: string;
+  stack?: string;
+};
+export type OpenAiSuccessResponse = {
+  id: string;
+  object: string;
+  created: number;
+  model: string;
+  choices: Choice[];
+  usage: Usage;
+};
+
+export type OpenAiResponse = {
+  data?: OpenAiSuccessResponse;
+  status: string;
+  error?: OpenAiErrorResponse;
+};
