eb198bdfe5f5c12940a5aaa37efa2981b51e809f
Dina Berry (SHE/HER) <Dina.Berry@microsoft.com>
2023-06-10 05:31:39 -0700

    wip storage

---
+++ api-functions-v4-blob-storage/readme.md
@@ -0,0 +1,8 @@
+# Azure Functions v4 + Blob Storage
+
+## Install local development tools
+
+```bash
+npm install -g azure-functions-core-tools@4 --unsafe-perm true
+npm install -g azurite
+```
--- api-functions-v4-blob-storage/src/functions/upload.ts
+++ api-functions-v4-blob-storage/src/functions/upload.ts
@@ -4,86 +4,68 @@ import HTTP_CODES from "http-status-enum";
 // Multiform management
 import * as multipart from "parse-multipart";
 
-// Used to get read-only SAS token to return with URL
-import { generateReadOnlySASUrl } from '@azure-typescript-e2e-apps/lib-azure-sql';
+//type CheckRequiredParamsResult = { success: boolean, errors?: string[], body?: Record<string, any> };
 
-type CheckRequiredParamsResult = { success: boolean, errors?: string[], body?: Record<string, any> };
+// Used to get read-only SAS token URL
+import { generateReadOnlySASUrl } from './azure-storage-blob-sas-url';
 
-async function checkRequiredParams(request: HttpRequest):Promise<CheckRequiredParamsResult>{
-
-  let errors = [];
-
-  // get connection string to Azure Storage from environment variables
-  // Replace with DefaultAzureCredential before moving to production
-  const storageConnectionString = process.env.AzureWebJobsStorage;
-  if (!storageConnectionString) {
-    errors.push(`AzureWebJobsStorage env var is not defined - get Storage Connection string from Azure portal`);
+function returnResult({
+  status = 500,
+  body = {}
+}): HttpResponseInit {
+  return {
+    status,
+    jsonBody: body
   }
-
-  // User name is the container name
-  const containerName = request.query?.username;
-  if (!containerName) {
-    errors.push(`username is not defined`);
-  }
-
-  // `filename` is required property to use multi-part npm package
-  const fileName = request.query?.filename;
-  if (!fileName) {
-    errors.push(`filename is not defined`);
-  }
-
-  // file content must be passed in as body
-  const body = await request.json();
-  if (!body || !body.length) {
-    errors.push(`Request body is not defined`);
-  }
-
-  // Content type is required to know how to parse multi-part form
-  if (!request.headers || !request.headers["content-type"]) {
-    errors.push(`Content type is not sent in header 'content-type'`);
-  }
-  return { errors, body, success: errors.length === 0 };
 }
 
 export async function postUpload(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
 
   try {
 
-    context.log(`Http function processed request for url "${request.url}"`);
+    context.log('upload HTTP trigger function processed a request.');
 
-    // return errors if any
-    const result: CheckRequiredParamsResult = await checkRequiredParams(request);
-    if (!result.success) {
-      return {
-        status: HTTP_CODES.BAD_REQUEST,
-        body: result.errors
-      }
+    // get connection string to Azure Storage from environment variables
+    // Replace with DefaultAzureCredential before moving to production
+    const storageConnectionString = process.env.AzureWebJobsStorage;
+    if (!storageConnectionString) {
+      returnResult({ status: HTTP_CODES.BAD_REQUEST, body: { error: `AzureWebJobsStorage env var is not defined - get Storage Connection string from Azure portal` } })
     }
 
-    const userName = request.query?.username;
-    const fileName = request.query?.filename;
-    const containerName = userName;
+    // User name is the container name
+    const containerName = request.query.get('username');
+    if (!containerName) {
+      returnResult({ status: HTTP_CODES.BAD_REQUEST, body: { error: `username is not defined` } })
 
-    console.log(`*** Username:${request.query?.username}, Filename:${request.query?.filename}, Content type:${request.headers["content-type"]}, Length:${body.length}`);
+    }
 
-    // Each chunk of the file is delimited by a special string
-    const bodyBuffer = Buffer.from(result?.body as any);
-    const boundary = multipart.getBoundary(request.headers["content-type"]);
-    const parts = multipart.Parse(bodyBuffer, boundary);
+    // `filename` is required property to use multi-part npm package
+    const fileName = request.query.get('filename');
+    if (!fileName) {
+      returnResult({ status: HTTP_CODES.BAD_REQUEST, body: { error: `filename is not defined` } })
 
-    return {
-      status: 200,
+    }
 
-    };
+    // file content must be passed in as body
+    const body = await request.formData();
+    if (body) {
+      returnResult({ status: HTTP_CODES.BAD_REQUEST, body: { error: `Request body is not defined` } })
+    }
+
+    // Content type is required to know how to parse multi-part form
+    const contentType = request.headers.get("content-type");
+    if (!contentType) {
+      returnResult({ status: HTTP_CODES.BAD_REQUEST, body: { error: `Content type is not sent in header 'content-type'` } });
+    }
+
+
+    context.log(`*** Username:${containerName}, Filename:${fileName}, Content type:${contentType}, Length:${body}`);
+
+    return returnResult({ status: HTTP_CODES.OK, body: { message: `Upload successful` } });
+
   } catch (err) {
 
     console.log(err)
-    return {
-      status: 500,
-      body: {
-        err
-      }
-    }
   }
 };
 
