59726ddebc4e90c57c96d7cff8413e49abed581b
Dina Berry <dinaberry@outlook.com>
2023-07-14 14:25:11 -0700

    clean up

--- app-react-vite-file-upload/src/App.tsx
+++ app-react-vite-file-upload/src/App.tsx
@@ -1,16 +1,11 @@
 // @ts-nocheck
 
 import { useState, ChangeEvent } from 'react';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import Button from '@mui/material/Button';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import Typography from '@mui/material/Typography';
 import { BlockBlobClient } from '@azure/storage-blob';
 import ErrorBoundary from './components/error-boundary';
 
 import axios from 'axios';
+import './App.css';
 
 const request = axios.create({
   baseURL: '',
@@ -19,39 +14,29 @@ const request = axios.create({
   }
 });
 
-import sasTokenService from './services/sas-token';
-const darkTheme = createTheme({
-  palette: {
-    mode: 'dark'
-  }
-});
-
-// type SasTokenUrlInfoResponse = {
-//   fileName:string,
-//   sasTokenUrl: string
-// }
-
-// type SasInfoTypeResponse = {
-//   error?: any,
-//   sasUrls?: SasTokenUrlInfoResponse[]
-// }
-
 type SelectedFilesType = {
   [key: string]: File;
 };
 
 function App() {
   const [text, setText] = useState<string>('');
-  const [status, setStatus] = useState<string>('');
+  const [readFileStatus, setReadFileStatus] = useState<string>('');
   const [selectedFile, setSelectedFile] = useState<SelectedFilesType>({});
   const [selectedFileArrayBuffer, setSelectedFileArrayBuffer] = useState<
     ArrayBuffer | undefined
   >(undefined);
   const [sasTokenUrl, setSasTokenUrl] = useState<string>('');
+  const [uploadStatus, setUploadStatus] = useState<string>('');
 
   const handleFileSelection = (event) => {
     setSelectedFile(event?.target?.files[0]);
-    setStatus('File selected');
+
+    // reset
+    setText('');
+    setSelectedFileArrayBuffer(undefined);
+    setSasTokenUrl('');
+    setUploadStatus('');
+    setReadFileStatus('');
   };
 
   const handleFileRead = () => {
@@ -60,40 +45,44 @@ function App() {
 
       reader.onload = () => {
         const arrayBuffer = reader.result;
-        setStatus('File read');
+        setReadFileStatus('Successfully read file into ArrayBuffer');
         setSelectedFileArrayBuffer(arrayBuffer as ArrayBuffer);
 
-        // convert to text to see what was there
-        const text = new TextDecoder('utf-8').decode(
-          arrayBuffer as ArrayBuffer
-        );
-        setText(text);
-
-        // Use the array buffer (e.g., perform further processing or send to server)
-        console.log(text);
+        if (selectedFile?.name?.endsWith('.text')) {
+          // convert to text to see what was there
+          const text = new TextDecoder('utf-8').decode(
+            arrayBuffer as ArrayBuffer
+          );
+          setText(text);
+        }
       };
       reader.readAsArrayBuffer(selectedFile);
     }
   };
 
   const handleFileSasToken = () => {
-
     // timestamp
     const timestamp = new Date().getTime();
     const containerName = `upload-${timestamp}`;
     const permission = 'w'; //write
     const timerange = 5; //minutes
 
-    request.post(`http://localhost:7071/api/sas?file=${encodeURIComponent(selectedFile.name)}&permission=${permission}&container=${containerName}&timerange=${timerange}`, {
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    }).then((result:any) => {
+    request
+      .post(
+        `http://localhost:7071/api/sas?file=${encodeURIComponent(
+          selectedFile.name
+        )}&permission=${permission}&container=${containerName}&timerange=${timerange}`,
+        {
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        }
+      )
+      .then((result: any) => {
         setSasTokenUrl(result?.data?.url);
-        setStatus(`Sas url received ${result?.data?.url}`);
       })
       .catch((error) => {
-        setStatus(`Error getting sas token: ${error.message}`);
+        setSasTokenUrl(`Error getting sas token: ${error.message}`);
       });
   };
 
@@ -102,67 +91,49 @@ function App() {
     blockBlobClient
       .uploadData(selectedFileArrayBuffer)
       .then(() => {
-        setStatus('File finished uploading');
+        setUploadStatus('Successfully finished upload');
       })
       .catch((error) => {
-        setStatus(`File finished uploading with error : ${error.message}`);
+        setUploadStatus(`Failed to finish upload with error : ${error.message}`);
       });
   };
 
   return (
     <>
-      <ThemeProvider theme={darkTheme}>
-        <ErrorBoundary>
-          <CssBaseline />
+      <ErrorBoundary>
 
-          <Box
-            sx={{
-              fontSize: 30,
-              alignItems: 'center',
-              margin: ' 5em 5em 3em 10em',
-              border: '1px solid #ccc',
-              height: '400px'
-            }}
-          >
-            <TextField
-              name="upload-photo"
-              type="file"
-              onChange={handleFileSelection}
-            />
-            <hr></hr>
-            {selectedFile.name && (
-              <Button
-                variant="contained"
-                component="label"
-                onClick={handleFileRead}
-              >
-                Read {selectedFile.name}
-              </Button>
-            )}
-            {selectedFileArrayBuffer && (
-              <Button
-                variant="contained"
-                component="label"
-                onClick={handleFileSasToken}
-              >
-                Get sas token for {selectedFile.name}
-              </Button>
-            )}
-            {sasTokenUrl && (
-              <Button
-                variant="contained"
-                component="label"
-                onClick={handleFileUpload}
-              >
-                Upload {selectedFile.name}
-              </Button>
-            )}
-            {status}
-            <br></br>
-            Text: {text}
-          </Box>
-        </ErrorBoundary>
-      </ThemeProvider>
+        <div className="container select-file">
+        <input className="select-file" type="file" onChange={handleFileSelection} />
+        </div>
+        
+        {selectedFile.name && (
+          <div className="container get-file-buffer">
+            <button className="get-file-buffer " onClick={handleFileRead}>
+              Read file into Buffer
+            </button>
+            <div className="get-file-buffer status">{readFileStatus}<br></br>{text}</div>
+          </div>
+        )}
+
+        {selectedFileArrayBuffer && (
+          <div className="container get-sas-token">
+            <button className="get-sas-token" onClick={handleFileSasToken}>
+              Get sas token for {selectedFile.name}
+            </button>
+            <div className="get-sas-token status">
+              <div className="get-sas-token status text">{sasTokenUrl}</div>
+              </div>
+          </div>
+        )}
+        {sasTokenUrl && (
+          <div className="container upload">
+            <button className="upload" onClick={handleFileUpload}>
+              Upload {selectedFile.name}
+            </button>
+            <div className="upload status">{uploadStatus}</div>
+          </div>
+        )}
+      </ErrorBoundary>
     </>
   );
 }
