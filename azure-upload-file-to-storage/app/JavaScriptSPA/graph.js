a010f221dd1cd438ff3534b23466e6d83b1e0b06
SymphonyOfSickles <philip.lapat@proton.me>
2023-08-13 15:31:57 +0000

    login

---
+++ azure-upload-file-to-storage/app/JavaScriptSPA/graph.js
@@ -0,0 +1,18 @@
+function callMSGraph(endpoint, token, callback) {
+    const headers = new Headers();
+    const bearer = `Bearer ${token}`;
+  
+    headers.append("Authorization", bearer);
+  
+    const options = {
+        method: "GET",
+        headers: headers
+    };
+  
+    console.log('request made to Graph API at: ' + new Date().toString());
+  
+    fetch(endpoint, options)
+      .then(response => response.json())
+      .then(response => callback(response, endpoint))
+      .catch(error => console.log(error))
+  }
