7fac2626a1085643b3c7d5ff7e752288475a3a24
Kars130700 <39276875+Kars130700@users.noreply.github.com>
2024-02-05 14:32:12 +0000

    commit to go further locally

--- azure-upload-file-to-storage/app/src/components/dragAndDrop.tsx
+++ azure-upload-file-to-storage/app/src/components/dragAndDrop.tsx
@@ -43,7 +43,7 @@ function DragDropFile() {
     };
     
     return (
-      <form id="form-file-upload" onDragEnter={handleDrag} onSubmit={(e) => e.preventDefault()}>
+      <form id="form-file-upload" onDragEnter={handleDrag} onSubmit={(e) => e.preventDefault()} onDrop={handleDrop}>
         <input ref={inputRef} type="file" id="input-file-upload" multiple={true} onChange={handleChange} />
         <label id="label-file-upload" htmlFor="input-file-upload" className={dragActive ? "drag-active" : "" }>
           <div>
@@ -51,7 +51,6 @@ function DragDropFile() {
             <button className="upload-button" onClick={onButtonClick}>Upload a file</button>
           </div> 
         </label>
-        { dragActive && <form id="drag-file-element" onDragEnter={handleDrag} onDragLeave={handleDrag} onDragOver={handleDrag} onDrop={handleDrop}></form> }
       </form>
     );
   }
