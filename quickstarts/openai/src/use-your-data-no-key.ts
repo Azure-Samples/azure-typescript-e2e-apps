e7fccb69496bae1f5d77c72d3d3efe35be6e8c02
Dina Berry (MSFT) <diberry@microsoft.com>
2024-09-10 13:51:17 -0700

    Update quickstarts/openai/src/use-your-data-no-key.ts
    Co-authored-by: Deyaaeldeen Almahallawi <dealmaha@microsoft.com>

--- quickstarts/openai/src/use-your-data-no-key.ts
+++ quickstarts/openai/src/use-your-data-no-key.ts
@@ -8,9 +8,8 @@ import "dotenv/config";
 
 // Set the Azure and AI Search values from environment variables
 const endpoint = process.env["AZURE_OPENAI_ENDPOINT"];
-const azureApiKey = process.env["AZURE_OPENAI_API_KEY"];
+const deploymentName = "gpt-4";
 
-const deploymentId = process.env["AZURE_OPENAI_DEPLOYMENT_ID"];
 
 const searchEndpoint = process.env["AZURE_AI_SEARCH_ENDPOINT"];
 const searchKey = process.env["AZURE_AI_SEARCH_API_KEY"];
