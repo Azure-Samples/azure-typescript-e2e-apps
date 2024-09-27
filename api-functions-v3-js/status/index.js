21d3bb4ce9a8f6e13c24a32e23f59d6aa869ec7b
Dina Berry <diberry@microsoft.com>
2024-09-27 09:30:53 -0700

    Remove old code

--- api-functions-v3-js/status/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: process.env
-    };
-}
--- api-functions-v4-cosmos-db-no-sql/README.md
+++
@@ -1,3 +0,0 @@
-## Uses Contoso CosmosWorks Data
-
-* [Contoso CosmosWorks Product.json](https://github.com/AzureCosmosDB/CosmicWorksJava/blob/main/src/main/java/com/azure/cosmos/examples/data/cosmic-works-v4/product)
